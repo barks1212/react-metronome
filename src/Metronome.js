936cbc0fdc71007fd6943b6ce26efe654a17c9e8
Will Barker <willbarkerwork@gmail.com>
2018-10-09 15:05:14 +0100

    basic ui

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 
-class App extends Component {
+class Metronome extends Component {
   render() {
-    return <div>yo</div>;
+    let bpm = 100;
+    let playing = false;
+
+    return (
+      <div className="metronome">
+        <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <input type="range" min="60" max="240" value={bpm} />
+        </div>
+        <button>{playing ? "Stop" : "Start"}</button>
+      </div>
+    );
   }
 }
 
-export default App;
+export default Metronome;
